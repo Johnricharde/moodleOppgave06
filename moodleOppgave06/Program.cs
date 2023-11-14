21a0abe52ae09aa7b1c0009db6a6fd5838b2ca04
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-14 13:05:12 +0100

    Add project files.

---
+++ moodleOppgave06/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace moodleOppgave06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Another one...");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            static bool checkkIfEqual(int num1, int num2)
+            {
+                if (num1 == num2)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            Console.WriteLine($"Are these numbers equal?: {checkkIfEqual(num1, num2)}");
+        }
+    }
+}
